f1909b1854b8a06578a44f814007fe835593f16d
steve <stefan.hadbavny@student.tuke.sk>
2022-04-27 15:43:38 +0200

    ivertory2

--- hackathon-2022/hraGodot/World/Tree.gd
+++ hackathon-2022/hraGodot/World/Tree.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
-<<<<<<< HEAD
-onready var labelCountWood = get_parent().get_parent().get_node("Player/Camera2D/HBoxContainer/WoodsCount")
-=======
 
->>>>>>> 584f50c8868a4a70cb4263121ecc3c61577d5cfa
+onready var labelCountWood = get_parent().get_parent().get_node("Player/Camera2D/HBoxContainer/WoodsCount")
+
+
+
 onready var player = get_parent().get_parent().get_parent().get_node("YSort/Player")
 onready var tree = get_node("Sprite")
 var hp_left = 2
