d6ff4fe36787be99fe1411025a29ea3af3d0c2df
alexandra javorska <alexandra.javorska@student.tuke.sk>
2022-04-28 19:27:37 +0200

    asteroid

---
+++ hackathon-2022/hraGodot/World/AsteroidRock.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+onready var player = get_parent().get_parent().get_parent().get_node("YSort/Player")
+onready var countAsteroids = get_parent().get_parent().get_parent().get_node("HealthUI/CanvasLayer/Panel/AsteroidCount")
+onready var asteroid = get_node("Sprite")
+var hp_left = 2
+
+
+
+func _on_Hurtbox_area_entered(area):
+	if player.pickaxe == true:
+		asteroid.scale.x *= 0.75
+		asteroid.scale.y *= 0.75
+		hp_left -= 1
+		if hp_left == 0:
+			player.asteroids+=1
+			countAsteroids.text=str(player.asteroids)
+			queue_free()
