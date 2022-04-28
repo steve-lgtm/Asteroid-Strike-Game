692f89ab217768303e932e030964a9b5644968e2
Miroslav Kapa <miroslav.kapa@student.tuke.sk>
2022-04-28 23:57:21 +0200

    pre istotu

--- hackathon-2022/hraGodot/Map.gd
+++ hackathon-2022/hraGodot/Map.gd
@@ -6,3 +6,4 @@ func _on_BrigePreparation_changeTiles():
 	set_cell(8,2,13)
 	set_cell(9,2,11)
 	set_cell(10,2,12)
+
--- hackathon-2022/hraGodot/World/BrigePreparation.gd
+++ hackathon-2022/hraGodot/World/BrigePreparation.gd
@@ -2,6 +2,8 @@ extends StaticBody2D
 
 
 onready var labelVar = $Label
+
+onready var bridgeWin = get_parent().get_parent().get_node("BridgeBuild/CanvasLayer/Panel")
 onready var player = get_parent().get_parent().get_node("YSort/Player")
 var buildArea = false
 
@@ -10,10 +12,16 @@ signal changeTiles
 
 
 func _process(delta):
-	if Input.is_action_just_pressed("use") and buildArea and player.rocks == 5 and player.woods == 10:
-		emit_signal("changeTiles")
-		queue_free()
-
+	if Input.is_action_just_pressed("use") and buildArea:
+		if player.rocks == 5 and player.woods == 10:
+			player.rocks -= 5
+			player.woods -= 10
+			player.countRock.text=str(player.rocks)
+			player.countWood.text=str(player.woods)
+			emit_signal("changeTiles")
+			queue_free()
+		else:
+			bridgeWin.visible = true
 
 func _on_Area2D_body_entered(body):
 	if body.name == "Player":
@@ -26,3 +34,7 @@ func _on_Area2D_body_exited(body):
 		labelVar.visible = false
 		buildArea = false
 	
+
+
+func _on_Button_pressed():
+	bridgeWin.visible = false
